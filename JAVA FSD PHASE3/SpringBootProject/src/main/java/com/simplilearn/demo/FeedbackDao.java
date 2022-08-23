22f9bd7e88eecc353abcca95d1f54ed21a1bc9cb
prashanthcurry <prash.spp@gmail.com>
2022-08-23 21:38:47 +0530

    phase 3

---
+++ JAVA FSD PHASE3/SpringBootProject/src/main/java/com/simplilearn/demo/FeedbackDao.java
@@ -0,0 +1,18 @@
+package com.simplilearn.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FeedbackDao {
+	
+	@Autowired
+	private JdbcTemplate template;
+	
+	public int addFedback(FeedbackEntity feedback) {
+		
+		return template.update("insert into student(name,email,feedback) values (?,?,?)", 
+				feedback.getName(),feedback.getEmail(),feedback.getFeedback());
+	}
+}
